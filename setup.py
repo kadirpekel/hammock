6ee8f094dab499376d4add6100c9204352aa566a
kadirpekel <kadirpekel@gmail.com>
2012-08-13 14:51:22 +0300

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'hammock',
     packages = ['hammock'],
-    version = '0.0.4',
+    version = '0.0.5',
     description = 'rest like a boss',
     author='Kadir Pekel',
     author_email='kadirpekel@gmail.com',
