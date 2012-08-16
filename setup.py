3b58f16cb238cd97426d111fe78b1b689162400e
Michele Lacchia <michelelacchia@gmail.com>
2012-08-16 13:31:24 +0200

    Changed project layout (package to module)

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
  
 setup(
-    name = 'hammock',
-    packages = ['hammock'],
-    version = '0.1.0',
-    description = 'rest like a boss',
+    name='hammock',
+    py_modules=['hammock'],
+    version='0.1.0',
+    description='rest like a boss',
     author='Kadir Pekel',
     author_email='kadirpekel@gmail.com',
     url='https://github.com/kadirpekel/hammock',
